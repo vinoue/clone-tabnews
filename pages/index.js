4f5dcfaa0baaabf9eeaf355b91fc2c24837aa178
vitor inoue <sickey@gmail.com>
2024-03-29 19:03:56 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home() {
-    return <h1>Eu amo voce! &#x1F60E; </h1>
+  return <h1>Eu amo voce! &#x1F60E; </h1>;
 }
 
+function Teste() {
+  console.log("interação errada");
+}
 export default Home;
